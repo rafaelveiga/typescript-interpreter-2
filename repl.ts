096ec652ca4214cdf3bd2ebc1742e00fb16fea29
Rafael Veiga <dsg.rafael@gmail.com>
2024-05-27 21:26:27 -0300

    update repl to use AST

--- repl.ts
+++ repl.ts
@@ -1,19 +1,31 @@
 import Lexer from "./lexer";
 
 import readline from "readline";
+import Parser from "./parser";
 
 const rs = readline.createInterface({
   input: process.stdin,
 });
 
+const OOPS = `
+ ____  ____  ____  ____
+/  _ \/  _ \/  __\/ ___\
+| / \|| / \||  \/||    \
+| \_/|| \_/||  __/\___ |
+\____/\____/\_/   \____/
+`;
+
 rs.on("line", (input) => {
   const tokenizer = new Lexer(input);
+  const parser = new Parser(tokenizer);
 
-  while (true) {
-    const token = tokenizer.nextToken();
-    console.log(token);
-    if (token.type === "EOF") {
-      break;
-    }
+  const program = parser.parseProgram();
+
+  if (parser.errors.length > 0) {
+    console.log(OOPS);
+    console.log("parser errors", parser.errors);
+    return;
   }
+
+  console.log(program.string());
 });
